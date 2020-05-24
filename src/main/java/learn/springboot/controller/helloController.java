d7e344c68ed26cb99f1f1d7d11324ead5c77ec85
Pranav <naman332@gmail.com>
2020-05-24 02:29:09 +0100

    Initial commit

---
+++ src/main/java/learn/springboot/controller/helloController.java
@@ -0,0 +1,16 @@
+package learn.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class helloController {
+
+    @RequestMapping("/sayHello")
+    @ResponseBody
+    String sayHello(){
+        return "Hello World from Spring boot application - Maven build";
+    }
+
+}
