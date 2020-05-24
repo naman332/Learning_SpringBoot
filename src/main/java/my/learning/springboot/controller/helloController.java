a7af2881af717ccc8909efa8c18f6453479841b2
Pranav <naman332@gmail.com>
2020-05-24 02:37:53 +0100

    updated the package name

--- src/main/java/my/learning/springboot/controller/helloController.java
+++ src/main/java/my/learning/springboot/controller/helloController.java
@@ -1,4 +1,4 @@
-package learn.springboot.controller;
+package my.learning.springboot.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
